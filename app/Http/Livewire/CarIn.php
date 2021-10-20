4399ff1b5773dd3ea6f306a09493aa33a82ff1da
Mohammed Fahad <H4ck3r@Mohammeds-MacBook-Pro.local>
2021-10-20 23:44:05 +0300

    created new client form and lookup clients

--- app/Http/Livewire/CarIn.php
+++ app/Http/Livewire/CarIn.php
@@ -9,12 +9,18 @@ class CarIn extends Component
 {
     public $showCarInForm = false;
     public $clientPhone = '';
+    public $clientName = '';
     public $lookedUpClient = null;
+    public $step = 1;
+    public $newClientPhone = '';
+    public $newClientName = '';
+
 
     public function updatedShowCarInForm()
     {
         $this->clientPhone = '';
         $this->lookedUpClient = null;
+        $this->step = 1;
     }
 
     public function updatedClientPhone()
@@ -27,11 +33,40 @@ class CarIn extends Component
     public function lookupClientPhone($clientPhone)
     {
         $client = Client::where('phone', $clientPhone)->first();
-        $this->lookedUpClient = $client;
+        if ($client !== null) {
+            $this->lookedUpClient = $client;
+        } else {
+            $this->step = 2;
+            $this->newClientPhone = $this->clientPhone;
+            $this->clientPhone = '';
+        }
+    }
+
+
+    public function signUpNewClient()
+    {
+        $client = Client::create(
+            [
+                'name' => $this->newClientName,
+                'phone' => $this->newClientPhone
+            ]
+        );
+        if ($client) {
+            $this->clientPhone = $client->phone;
+            $this->cleanClientData();
+            $this->step = 1;
+        }
     }
 
     public function render()
     {
         return view('livewire.car-in');
+    }
+
+    protected function cleanClientData()
+    {
+        $this->lookedUpClient = null;
+        $this->newClientPhone = '';
+        $this->newClientName = '';
     }
 }
--- resources/views/livewire/car-in.blade.php
+++ resources/views/livewire/car-in.blade.php
@@ -4,7 +4,7 @@
         <button class="bg-red-300 text-lg text-white font-bold rounded p-10">Car Out</button>
     </div>
 
-    @if($showCarInForm)
+    @if($showCarInForm && $step == 1)
     <div class="flex items-center p-6 animate__animated animate__fadeIn">
         <div class="flex flex-col">
             <div class="flex flex-row items-center mb-3">
@@ -22,7 +22,7 @@
             @if($lookedUpClient !== null)
             <div class="flex flex-row items-center mb-3">
                 <div>
-                    <div class="flex flex-row items-center justify-around">
+                    <div class="flex flex-row items-center">
                         <x-jet-label class="mr-3" for="name" value="{{ __('Client Name:') }}" />
                         <p class="text-sm font-semibold text-gray-800 tracking-wide">{{ $lookedUpClient->name }}</p>
                     </div>
@@ -35,6 +35,28 @@
             </div>
             @endif
 
+        </div>
+    </div>
+    @elseif($step === 2)
+    <div class="flex items-center p-6 animate__animated animate__fadeIn">
+        <div class="flex flex-col">
+            <div class="flex flex-col items-center mb-3">
+                <div>
+                    <x-jet-label for="newClientName" value="{{ __('Client Name') }}" />
+                    <x-jet-input wire:model="newClientName" size="50" id="newClientName" class="block mt-1 w-full" type="text" :value="old('newClientName')" placeholder="Client's Name" required />
+                </div>
+
+                <div class="mt-3">
+                    <x-jet-label for="newClientPhone" value="{{ __('Client Phone Number') }}" />
+                    <x-jet-input wire:model="newClientPhone" size="50" id="newClientPhone" class="block mt-1 w-full" type="text" :value="old('newClientPhone')" placeholder="Client's Phone Number" required />
+                </div>
+
+                @if(!empty($newClientName) && !empty($newClientPhone))
+                <x-jet-button class="ml-4 mt-6" wire:click="signUpNewClient">
+                    {{ __('Sign up') }}
+                </x-jet-button>
+                @endif
+            </div>
         </div>
     </div>
     @endif
